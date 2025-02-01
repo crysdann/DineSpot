a4baa900f73d627d719b6a676963e321df523b18
CrystalGarcia <crysdann12@gmail.com>
2025-02-01 16:03:34 -0500

    Displaying the details page

--- app/restaurants/[slug]/page.js
+++ app/restaurants/[slug]/page.js
@@ -1,7 +1,31 @@
-import React from "react";
+import Image from "next/image";
 
-function page({ params }) {
-  return <div>Meals slug</div>;
+import { getRestaurant } from "@/lib/restaurants";
+import classes from "./page.module.css";
+
+function RestaurantDetailsPage({ params }) {
+  const restaurant = getRestaurant(params.slug);
+  restaurant.description = restaurant.description.replace(/\n/g, "<br/>");
+  console.log("restaurant: ", restaurant);
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={restaurant.image} alt={restaurant.name} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{restaurant.name}</h1>
+          {/* <p className={classes.creator}>{restaurant.description}</p> */}
+          <p className={classes.address}>{restaurant.address}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: restaurant.description }}></p>
+      </main>
+    </>
+  );
 }
 
-export default page;
+export default RestaurantDetailsPage;
